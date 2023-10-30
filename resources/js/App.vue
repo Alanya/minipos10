6f97304ec33a14a44266531cdbfb387f9c78b1c6
unknown <olaykhamchalern@gmail.com>
2023-10-30 17:24:57 +0700

    upload to github

---
+++ resources/js/App.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h1>Hello Vuejs App2023</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'WenprojectApp',
+
+    data() {
+        return {
+            
+        };
+    },
+
+    mounted() {
+        
+    },
+
+    methods: {
+        
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
