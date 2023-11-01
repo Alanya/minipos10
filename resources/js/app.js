3134e3af9a14a13b7df2845219d5a0125c237dbe
unknown <olaykhamchalern@gmail.com>
2023-11-01 16:56:31 +0700

    POS1 && git push

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,11 @@
 import './bootstrap';
+
 import { createApp } from 'vue';
 import App from './App.vue';
+import router from './router';
+import Menu from './components/Memu.vue'
 
 const app = createApp(App)
+app.component('Menubar',Menu);
+app.use(router);
 app.mount('#app-vue');
